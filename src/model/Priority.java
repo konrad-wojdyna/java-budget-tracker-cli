e0fb0f90e74cddf3ce226df1dccbb14426d28ad6
konrad-wojdyna <konrad.wojdyna@gmail.com>
2025-11-30 15:40:07 +0100

    feat(model): add Priority enum for expense importance levels

---
+++ src/model/Priority.java
@@ -0,0 +1,17 @@
+package model;
+
+
+/**
+ * Represents expense priority levels.
+ *
+ * Used to categorize expenses by urgency or importance.
+ *
+ * @author Konrad Wojdyna
+ * @version 0.3.0
+ */
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH,
+    URGENT
+}
